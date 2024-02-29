01ae44a85e6cc5fe58d1a2c17b0c3dbb933ffc85
jaimehu20 <jaimehu20@gmail.com>
2024-03-01 00:47:14 +0100

    adding js, sending info to json server finished

--- scripts/script.js
+++ scripts/script.js
@@ -36,7 +36,6 @@ window.addEventListener("scroll", () => {
     windowHeight = document.body.scrollHeight - window.innerHeight;
     let percent = (scrollIndex / windowHeight) * 100;
     document.querySelector('#scrollBar').style.width = percent + "%";
-
 })
 
 // RETURN TO TOP BUTTON
@@ -58,62 +57,138 @@ const email = document.getElementById('email');
 const checkbox = document.getElementById('formCheck');
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
+let nombreChecked = false;
 nombre.addEventListener("input", () => {
-    if (nombre.value.length < 2 || nombre.value.length > 100) {
-        nombre.classList.add("contactSection__consenting__contactData__form__input--error");
-        
+    if (nombre.value.length <= 2 || nombre.value.length >= 100){
+        nombre.classList.add('contactSection__consenting__contactData__form__input--error');
     } else {
         nombre.classList.remove('contactSection__consenting__contactData__form__input--error');
+        nombreChecked = true;
     }
 })
 
-checkbox.addEventListener("input", () => {
+let emailChecked = false;
+ email.addEventListener("input", () => {
+    if (!regex.test(email.value)) {
+        email.classList.add('contactSection__consenting__contactData__form__input--error');
+    } else {
+        email.classList.remove('contactSection__consenting__contactData__form__input--error');
+        emailChecked = true;
+    }
+ })
+
+ let isChecked = false;
+checkbox.addEventListener("click", () => {
     if (checkbox.checked) {
-        checkbox.classList.add('contactSection__consenting__contactData__form__checkbox--error');
-    } else {
-        checkbox.classList.remove('contactSection__consenting__contactData__form__checkbox--error');
+        isChecked = true;
     }
 })
 
-function validateEmail(userMail, tester) {
-    if (tester.test(userMail.value)) {
-        return userMail.value;
+myForm.addEventListener("submit", (event) => {
+    if (nombreChecked === true) {
+        console.log("Nombre correcto, listo para enviar.");
     } else {
-        userMail.classList.add('contactSection__consenting__contactData__form__input--error');
-        
+        alert("The name entered is not valid!");
     }
-}
-
-email.addEventListener("input", () => {
-    validateEmail(email, regex);
-})
-
-form.addEventListener("submit", (event) => {
-    const checkBoxOk = checkbox.checked;
-    const nombreOk = nombre.value.length >= 2 && nombre.value.length <= 100;
-    const emailOk = regex.test(email.value);
-    event.preventDefault()
-
-    if (!checkBoxOk) {
-        checkbox.classList.add('contactSection__consenting__contactData__form__checkbox--error');
+    if (emailChecked === true) {
+        console.log("Email correcto, listo para enviar.");
+    } else {
+        alert("The email entered is not valid!");
+    }
+    if (isChecked === true) {
+        console.log("Términos aceptados, listo para enviar.");
+    } else {
+        alert("You must accept the terms to continue!");
     }
 
-    if (!nombreOk) {
-        nombre.classList.add("contactSection__consenting__contactData__form__input--error");
-        alert("El nombre introducido no es válido.")
+    if(nombreChecked && emailChecked && isChecked) {
+        fetchUserInfo(nombre, email, checkbox)
+        form.reset()
     }
-
-    if (!emailOk) {
-        userMail.classList.add('contactSection__consenting__contactData__form__input--error');
-        alert("El email introducido no es correcto.");
-    }
-
-// SEGUIR POR AQUI MAÑANA
+    event.preventDefault();
 })
 
 document.addEventListener("DOMContentLoaded", function() {
-    myForm.reset()
+    form.reset()
 });
+
+// ENVIO DE DATOS AL SERVIDOR JSON
+
+async function fetchUserInfo(userName, userMail, userCheckbox) {
+    try {
+        let response = {}
+        if (userName && userMail && userCheckbox) {
+            response = await fetch('https://jsonplaceholder.typicode.com/users', {
+                method: 'POST',
+                body: JSON.stringify({
+                    nombre: userName.value,
+                    email: userMail.value,
+                    checkbox: userCheckbox.checked,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                  },
+            })
+        }
+        if (!response.ok){
+            throw new error ('Server returned ' + response.status + ' status code.')
+        } else {
+            const data = await response.json()
+            if (data.nombre && data.email) {
+                alert("Information sent succesfully!");
+            }
+        }
+    }
+    catch (error) {
+        throw new error ('Something went wrong: ' + error.message);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // POPUP NEWSLETTER
@@ -137,3 +212,6 @@ document.addEventListener("keydown", press => {
     }
 })
 
+const scrollPercent = document.getElementById('scrollBar');
+
+console.log()
