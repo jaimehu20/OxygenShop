9c3079f610a534f122cfeffeaa7cd4498daa072d
jaimehu20 <jaimehu20@gmail.com>
2024-03-06 17:55:32 +0100

    fixed scrollbar

--- scripts/formValidation__&__popUp.js
+++ scripts/formValidation__&__popUp.js
@@ -114,15 +114,7 @@ function showPopUp() {
     }, 5000);
 }
 
-window.addEventListener("scroll", () => {
-    let scrollIndex = window.scrollY;
-    windowHeight = document.body.scrollHeight - window.innerHeight;
-    let percent = (scrollIndex / windowHeight) * 100;
-    document.querySelector('#scrollBar').style.width = percent + "%";
-    if(percent > 25 && !localStorage.getItem('popUpClosed')){
-        popUpBox.classList.add('openingSection__popUp--enabled')
-    }
-})
+
 
 window.onload = () => {
     if (!localStorage.getItem('popUpClosed')){
--- scripts/scrollBar.js
+++ scripts/scrollBar.js
@@ -1,2 +1,10 @@
-
+window.addEventListener("scroll", () => {
+    let scrollIndex = window.scrollY;
+    windowHeight = document.body.scrollHeight - window.innerHeight;
+    let percent = (scrollIndex / windowHeight) * 100;
+    document.querySelector('#scrollBar').style.width = percent + "%";
+    if(percent > 25 && !localStorage.getItem('popUpClosed')){
+        popUpBox.classList.add('openingSection__popUp--enabled');
+    }
+})
 
